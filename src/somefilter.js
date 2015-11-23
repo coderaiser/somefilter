487be593d852d5a94b71f707298b8b7652788d05
coderaiser <mnemonic.enemy@gmail.com>
2015-11-23 13:30:42 -0500

    feature(somefilter) add

---
+++ src/somefilter.js
@@ -0,0 +1,84 @@
+'use strict';
+
+let squad       = require('squad');
+let apart       = require('apart');
+
+let unary       = fn => value => fn(value);
+let notEmpty    = value => !!value;
+
+module.exports = somefilter;
+
+function somefilter(condition, filters) {
+    if (!filters) {
+        filters     = condition;
+        condition   = notEmpty;
+    }
+    
+    checkAll(condition, filters);
+    
+    let storify = store(condition);
+    let process = apart(squad, condition, storify);
+    
+    let processingFilters = filters
+        .map(unary(process))
+        .reverse();
+    
+    return function() {
+        processingFilters
+            .some(filter =>
+                filter(...arguments)
+            );
+        
+        return storify();
+    };
+}
+
+function store(condition) {
+    let cache;
+   
+    if (!condition)
+       condition = notEmpty;
+    
+    return value => {
+       let result;
+       
+        if (condition(value)) {
+            cache   =
+            result  = value;
+        } else {
+            result  = cache;
+            cache   = null;
+        }
+       
+        return result;
+   };
+}
+
+function checkAll(condition, filters) {
+    if (typeof condition !== 'function')
+        throw Error('condition should be function!');
+    
+    if (!Array.isArray(filters))
+        throw Error('filters should be an array!');
+    
+    check('function', filters);
+}
+
+function check(type, array) {
+    let getType     = item => typeof item,
+        notEqual    = (a, b) => a !== b,
+        wrong       = type => {
+            throw Error(`fn should be ${ type }!`);
+        },
+        
+        wrongType   = apart(wrong, type),
+        notType     = apart(notEqual, type);
+    
+    if (!array.length)
+        wrongType(type);
+    else
+        array
+            .map(getType)
+            .filter(notType)
+            .forEach(wrongType);
+}
---
+++ test/somefilter.js
@@ -0,0 +1,54 @@
+(function() {
+    'use strict';
+    
+    let somefilter  = require('../src/somefilter');
+    let apart       = require('apart');
+    let test        = require('tape');
+    
+    let add     = (a, b) => a + b;
+    let less    = (a, b) => a < b;
+    
+    let sub10   = apart(add, -10);
+    let sub5    = apart(add, -5);
+    let more6   = apart(less, 6);
+    
+    test('return result of first function', t => {
+        let find = somefilter([sub10, sub5]);
+        
+        t.deepEqual(find(6), 1);
+        t.end();
+    });
+    
+    test('return result of second function', t => {
+        let find = somefilter([sub10, sub5]);
+        
+        t.deepEqual(find(5), -5);
+        t.end();
+    });
+    
+    test('return result according to condition', t => {
+        let find = somefilter(more6, [sub5, sub10]);
+        
+        t.deepEqual(find(12), 7);
+        t.end();
+    });
+    
+    test('arguments: no', t => {
+        t.throws(somefilter, /filters should be an array!/, 'should throw when no filters');
+        t.end();
+    });
+    
+    test('arguments: no filters', t => {
+        let fn  = () => somefilter(more6);
+       
+        t.throws(fn, /filters should be an array!/, 'should throw when no filters');
+        t.end();
+    });
+    
+    test('arguments: condition not a function', t => {
+        let fn  = () => somefilter([], []);
+       
+        t.throws(fn, /condition should be function!/, 'should throw when condition not function');
+        t.end();
+    });
+})();
