6dadd6eb6e3a8609fe1dbd57e52b49ccee23dff0
coderaiser <mnemonic.enemy@gmail.com>
2016-04-15 12:28:06 -0400

    feature(package) jscs v3.0.1

--- test/somefilter.js
+++ test/somefilter.js
@@ -1,68 +1,66 @@
-(function() {
-    'use strict';
+'use strict';
+
+let somefilter  = require('../src/somefilter');
+let apart       = require('apart');
+let test        = require('tape');
+
+let add     = (a, b) => a + b;
+let less    = (a, b) => a < b;
+
+let sub10   = apart(add, -10);
+let sub5    = apart(add, -5);
+let more6   = apart(less, 6);
+
+test('return result of first function', t => {
+    let find = somefilter([sub10, sub5]);
     
-    let somefilter  = require('../src/somefilter');
-    let apart       = require('apart');
-    let test        = require('tape');
+    t.deepEqual(find(6), 1);
+    t.end();
+});
+
+test('return result of second function', t => {
+    let find = somefilter([sub10, sub5]);
     
-    let add     = (a, b) => a + b;
-    let less    = (a, b) => a < b;
+    t.deepEqual(find(5), -5);
+    t.end();
+});
+
+test('return result according to condition', t => {
+    let find = somefilter(more6, [sub5, sub10]);
     
-    let sub10   = apart(add, -10);
-    let sub5    = apart(add, -5);
-    let more6   = apart(less, 6);
-    
-    test('return result of first function', t => {
-        let find = somefilter([sub10, sub5]);
-        
-        t.deepEqual(find(6), 1);
-        t.end();
-    });
-    
-    test('return result of second function', t => {
-        let find = somefilter([sub10, sub5]);
-        
-        t.deepEqual(find(5), -5);
-        t.end();
-    });
-    
-    test('return result according to condition', t => {
-        let find = somefilter(more6, [sub5, sub10]);
-        
-        t.deepEqual(find(12), 7);
-        t.end();
-    });
-    
-    test('arguments: no', t => {
-        t.throws(somefilter, /filters should be an array!/, 'should throw when no filters');
-        t.end();
-    });
-    
-    test('arguments: no filters', t => {
-        let fn  = () => somefilter(more6);
-       
-        t.throws(fn, /filters should be an array!/, 'should throw when no filters');
-        t.end();
-    });
-    
-    test('arguments: condition not a function', t => {
-        let fn  = () => somefilter([], []);
-       
-        t.throws(fn, /condition should be function!/, 'should throw when condition not function');
-        t.end();
-    });
-    
-    test('arguments: filter on a function', t => {
-        let fn  = () => somefilter([() => {}, 'not a function']);
-       
-        t.throws(fn, /fn should be function!/, 'should throw when filter not a function');
-        t.end();
-    });
-    
-    test('arguments: empty array of filters', t => {
-        let fn  = () => somefilter([]);
-       
-        t.throws(fn, /fn should be function!/, 'should throw when filter not a function');
-        t.end();
-    });
-})();
+    t.deepEqual(find(12), 7);
+    t.end();
+});
+
+test('arguments: no', t => {
+    t.throws(somefilter, /filters should be an array!/, 'should throw when no filters');
+    t.end();
+});
+
+test('arguments: no filters', t => {
+    let fn  = () => somefilter(more6);
+   
+    t.throws(fn, /filters should be an array!/, 'should throw when no filters');
+    t.end();
+});
+
+test('arguments: condition not a function', t => {
+    let fn  = () => somefilter([], []);
+   
+    t.throws(fn, /condition should be function!/, 'should throw when condition not function');
+    t.end();
+});
+
+test('arguments: filter on a function', t => {
+    let fn  = () => somefilter([() => {}, 'not a function']);
+   
+    t.throws(fn, /fn should be function!/, 'should throw when filter not a function');
+    t.end();
+});
+
+test('arguments: empty array of filters', t => {
+    let fn  = () => somefilter([]);
+   
+    t.throws(fn, /fn should be function!/, 'should throw when filter not a function');
+    t.end();
+});
