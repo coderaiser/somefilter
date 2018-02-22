a3489f9cc81f09cc89935b42c874a65900e4b24d
coderaiser <coderaiser@cloudcmd.io>
2018-02-22 15:50:21 +0200

    chore(somefilter) let -> const

--- test/somefilter.js
+++ test/somefilter.js
@@ -1,32 +1,32 @@
 'use strict';
 
-let somefilter  = require('../src/somefilter');
-let apart       = require('apart');
-let test        = require('tape');
+const somefilter = require('../src/somefilter');
+const apart = require('apart');
+const test = require('tape');
 
-let add     = (a, b) => a + b;
-let less    = (a, b) => a < b;
+const add = (a, b) => a + b;
+const less = (a, b) => a < b;
 
-let sub10   = apart(add, -10);
-let sub5    = apart(add, -5);
-let more6   = apart(less, 6);
+const sub10 = apart(add, -10);
+const sub5 = apart(add, -5);
+const more6 = apart(less, 6);
 
 test('return result of first function', t => {
-    let find = somefilter([sub10, sub5]);
+    const find = somefilter([sub10, sub5]);
     
     t.deepEqual(find(6), 1);
     t.end();
 });
 
 test('return result of second function', t => {
-    let find = somefilter([sub10, sub5]);
+    const find = somefilter([sub10, sub5]);
     
     t.deepEqual(find(5), -5);
     t.end();
 });
 
 test('return result according to condition', t => {
-    let find = somefilter(more6, [sub5, sub10]);
+    const find = somefilter(more6, [sub5, sub10]);
     
     t.deepEqual(find(12), 7);
     t.end();
@@ -38,28 +38,28 @@ test('arguments: no', t => {
 });
 
 test('arguments: no filters', t => {
-    let fn  = () => somefilter(more6);
+    const fn  = () => somefilter(more6);
    
     t.throws(fn, /filters should be an array!/, 'should throw when no filters');
     t.end();
 });
 
 test('arguments: condition not a function', t => {
-    let fn  = () => somefilter([], []);
+    const fn  = () => somefilter([], []);
    
     t.throws(fn, /condition should be function!/, 'should throw when condition not function');
     t.end();
 });
 
 test('arguments: filter on a function', t => {
-    let fn  = () => somefilter([() => {}, 'not a function']);
+    const fn  = () => somefilter([() => {}, 'not a function']);
    
     t.throws(fn, /fn should be function!/, 'should throw when filter not a function');
     t.end();
 });
 
 test('arguments: empty array of filters', t => {
-    let fn  = () => somefilter([]);
+    const fn  = () => somefilter([]);
    
     t.throws(fn, /fn should be function!/, 'should throw when filter not a function');
     t.end();
